1ae35cf6f9a74e72400f0e344b6c271e601fe784
Paridhi Patidar <patidarparidhi35@gmail.com>
2025-02-21 23:11:07 +0530

    Add files via upload

---
+++ MajorityElement.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class MajorityElement {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n / 2];
+    }
+
+    public static void main(String[] args) {
+        MajorityElement solution = new MajorityElement();
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println("Majority Element: " + solution.majorityElement(nums));
+    }
+}
